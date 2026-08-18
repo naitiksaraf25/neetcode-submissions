471df74db1e366933acea38ff272fc6986b27a31
naitiksaraf25 <naitiksaraf2507@gmail.com>
2026-08-18 21:28:59 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/dynamicArray/submission-0.cpp
@@ -0,0 +1,47 @@
+class DynamicArray {
+public:
+    int*arr;
+    int capacity;
+    int size{};
+    DynamicArray(int capacity) {
+        this->capacity= capacity;
+        this->size=0;
+        this->arr = new int[capacity];
+    }
+
+    int get(int i) {
+        return arr[i];
+    }
+
+    void set(int i, int n) {
+        arr[i]=n;
+    }
+
+    void pushback(int n) {
+        if(size == capacity)resize();
+        arr[size++]=n;
+    }
+
+    int popback() {
+        return arr[--size];
+    }
+
+    void resize() {
+        capacity *= 2;
+        int* newArr= new int[capacity];
+        for(int i=0;i<size;i++)
+        {
+            newArr[i]=arr[i];
+        }
+        delete[]arr;
+        arr=newArr;
+    }
+
+    int getSize() {
+        return size;
+    }
+
+    int getCapacity() {
+        return capacity;
+    }
+};
